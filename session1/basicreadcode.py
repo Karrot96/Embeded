eed72d8d6fe7bebb5aaeaa9a7b48fb3fb4e38de4
JM3515 <35800419+JM3515@users.noreply.github.com>
2018-01-25 10:55:13 +0000

    Update basicreadcode.py

--- session1/basicreadcode.py
+++ session1/basicreadcode.py
@@ -1,1 +1,12 @@
+#import the necessary modules
+from machine import Pin,I2C 
+#creates the port i2c
+i2cPort=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+#sensor is generating data of size 0xF3 bytes and writing it to memory address 0x40
+i2cPort.writeto(0x40,bytearray([0xf3]))
+#reads 2 bytes of data from address 0x40
+data = i2cPort.readfrom(0x40,2)
+#converts the data into an int
+int.from_bytes(data,'big')
+
 

d9dc6a6af0bec093f6dc05419e4ee745aa3cde11
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-01-31 12:35:28 +0000

    Update basicreadcode.py

--- session1/basicreadcode.py
+++ session1/basicreadcode.py
@@ -14,35 +14,44 @@ def temp():
     #Since list we can then just use the locations in list and dynamically allocate
 
     address = i2cPort.scan()
+    
     i2cPort.writeto(address[0],bytearray([0xF3]))
+    
+    #give time for write
     time.sleep(0.020)
-    #reads 2 bytes of data from address 0x40
-    data = i2cPort.readfrom(address[0], 2)
-    blah = int.from_bytes(data,'big')
-    blah = (175.72*blah/65536)-46.85
-    #converts the data into an int
-    print(blah)
+    
+    #reads 2 bytes of raw data from address as detected by i2c port
+    tempRaw = i2cPort.readfrom(address[0], 2)
+    
+    #convert to integer
+    tempInt = int.from_bytes(tempRaw,'big')
+    
+    #convert raw value to real world value
+    temp = (175.72*tempInt/65536)-46.85
+    
+    print(temp)
 
 def humidity():
     i2cPort=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
     i2cPort.start()
-    #sensor is generating data of size 0xF3 bytes and writing it to memory address 0x40
-    #When we declare the address we should do it dynamically.
-    #I think this will fix the problem we are having
-    # http://docs.micropython.org/en/latest/wipy/library/machine.I2C.html
-    #Use the link of the i2c library.
-    #I think use of scan will be important
-    #Since list we can then just use the locations in list and dynamically allocate
 
     address = i2cPort.scan()
+    
     i2cPort.writeto(address[0],bytearray([0xE3]))
+    
+    #give time for write
     time.sleep(0.020)
-    #reads 2 bytes of data from address 0x40
-    data = i2cPort.readfrom(address[0], 2)
+    
+    #reads 2 bytes of raw data from address as detected by i2c port
+    humidityRaw = i2cPort.readfrom(address[0], 2)
+    
     #converts the data into an int
-    data = int.from_bytes(data,'big')
-    data = ((125*data)/65536)-6
-    print(str(data) + "%")
+    humidityInt = int.from_bytes(humidityRaw,'big')
+    
+    #convert raw value to real world value
+    humidity = ((125*humidityInt)/65536)-6
+    
+    print(str(humidity) + "%")
 
 temp()
 humidity()

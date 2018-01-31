b8e6b7f44c4b121ae802abc26bf5288daed95966
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-01-31 11:19:55 +0000

    Update basicreadcode.py

--- session1/basicreadcode.py
+++ session1/basicreadcode.py
@@ -1,7 +1,9 @@
 #import the necessary modules
 from machine import Pin,I2C
+
 #creates the port i2c
 i2cPort=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+
 #sensor is generating data of size 0xF3 bytes and writing it to memory address 0x40
 #When we declare the address we should do it dynamically.
 #I think this will fix the problem we are having
@@ -13,7 +15,9 @@ i2cPort=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 address = i2cPort.scan()
 
 i2cPort.writeto(address[0],bytearray([0xf3]))
-#reads 2 bytes of data from address 0x40
+
+#reads 2 bytes of data from first address
 data = i2cPort.readfrom(address[0],2)
+
 #converts the data into an int
 int.from_bytes(data,'big')

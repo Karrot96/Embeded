5891c8007ed1d4f21670090715aa432fde09f361
Karrot96-L <apk115@ic.ac.uk>
2018-02-05 15:01:45 +0000

    Fixed some constants

--- Code/CommInternet.py
+++ Code/CommInternet.py
@@ -1,13 +1,13 @@
 from umqtt.simple import MQTTClient
-
+import machine
 import network
 
 
 CLIENT_ID = machine.unique_id()
-
+BROKER_ADDRESS = '192.168.0.10'
 client = MQTTClient(CLIENT_ID,BROKER_ADDRESS)
 client.connect()
-client.publish(TOPIC,bytes(data,'utf-8'))
+#client.publish(TOPIC,bytes(data,'utf-8'))
 
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
@@ -16,4 +16,4 @@ sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect('EEERover', 'exhibition')
 
-isconnected()
+print (isconnected())

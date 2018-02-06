c89acce8ec9cae1fe13b9a1cc7fb38cc8da3ecfb
JM3515 <35800419+JM3515@users.noreply.github.com>
2018-02-06 17:05:20 +0000

    Update and rename subscibe.py to subscribe.py

--- Code/subscribe.py
+++ Code/subscribe.py
@@ -3,8 +3,8 @@ import machine
 from umqtt.simple import MQTTClient
 
 # Received messages from subscriptions will be delivered to this callback
-def sub_cb(topic, msg):
-    print((topic, msg))
+def sub_cb("ESYS/netball", msg):
+    print("ESYS/netball", msg))
 
 def sub_topic(server="localhost"):
     client  = MQTTClient(machine.unique_id(), '192.168.0.10')

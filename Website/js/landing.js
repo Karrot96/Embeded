0a8ab7dab8fbbcf17eb95e3643e57a29e18ae4f4
Karrot96-D <apk115@ic.ac.uk>
2018-02-07 00:12:21 +0000

    Started the menu

--- Website/js/landing.js
+++ Website/js/landing.js
@@ -1,7 +1,8 @@
 function landingClearFog(){
     x=document.getElementsByClassName('glass')
-    x[0].style.transform = 'translateY(-100vh)'
     x[0].style.background = 'rgba(217,214,209,0)'
     y=document.getElementsByClassName('FogOverlay')
     y[0].style.opacity = '0'
+    z=document.getElementsByClassName('menu')
+    z[0].style.display = 'block'
 }

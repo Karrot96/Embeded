625f1b6ecf856a5a9143859676a2c5f8c901ac7c
Karrot96-L <apk115@ic.ac.uk>
2018-02-08 10:29:28 +0000

    Working Main - Needs work still

--- Code/main.py
+++ Code/main.py
@@ -1,32 +1,15 @@
 #TO be the main code
-import machine
-import network
-import Sensor
 import CommInternet
-import ujson as json
-import time
-import network
-
-
-def DisableAp()
-def ConnectWifi()
-if not sta_if.isconnected():
-    print("not connected")
-    exit()
-def SendJson(data)
-=======
 import Json
-import ujson as json
 import time
-import network
-import subscribe
+
+
+
+CommInternet.DisableAp()
+CommInternet.ConnectWifi()
+#CommInternet.SendJson(data)
 
 while True:
-    CommInternet.DisableAp()
     Json.send()
-
     time.sleep(1)
-
-    subscribe.sub_topic(server="localhost")
-
-    time.sleep(300)
+    time.sleep(20)

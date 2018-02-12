b101aae77746d1110f6d6c028b8218346a8c9774
Jonny <Jm3515@ic.ac.uk>
2018-02-12 15:43:40 +0000

    j

--- Code/main.py
+++ Code/main.py
@@ -2,27 +2,18 @@ import CommInternet
 import Json
 import time
 import Sensor
-
+def debug():
+    debugJson = json.dumps({'name':'fog' , 'value':100 ,  'WindowFogged':True, 'RemoveFog': False})
+    CommInternet.SendJson(debugJson)
 #call relevant functions to set up wifi connection
 CommInternet.DisableAp()
-print("Connected")
 CommInternet.ConnectWifi()
-<<<<<<< HEAD
-print("Connected")
-
-#send data at periodic intervals
-while True:
-    #if Sensor.fog() == True:
-    Json.send()
-    time.sleep(20)
-=======
 text = input("enter 'd' to debug: ")
     if text = 'd'
-        debug
+        debug()
     else
     #send data at periodic intervals
         while True:
             #if Sensor.fog() == True:
             Json.send()
             time.sleep(20)
->>>>>>> 7f10631109ebd0da50af46a9a2924a50f72091d1

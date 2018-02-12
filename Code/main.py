7f10631109ebd0da50af46a9a2924a50f72091d1
Jonny <Jm3515@ic.ac.uk>
2018-02-12 15:35:35 +0000

    j

--- Code/main.py
+++ Code/main.py
@@ -6,10 +6,12 @@ import Sensor
 #call relevant functions to set up wifi connection
 CommInternet.DisableAp()
 CommInternet.ConnectWifi()
-
-#send data at periodic intervals
-while True:
-    #if Sensor.fog() == True:
-        Json.send()
-        time.sleep(20)
-    
+text = input("enter 'd' to debug: ")
+    if text = 'd'
+        debug
+    else
+    #send data at periodic intervals
+        while True:
+            #if Sensor.fog() == True:
+            Json.send()
+            time.sleep(20)

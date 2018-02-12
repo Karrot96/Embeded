2b941a634506c819062a27ff9a900d02fe9fb770
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-02-12 10:51:41 +0000

    Update Json.py

--- Code/Json.py
+++ Code/Json.py
@@ -2,9 +2,11 @@ import Sensor
 import CommInternet
 import ujson as json
 
+#convert data to json
 def jsonify():
     return json.dumps({'name':'humid1' , 'value': Sensor.humidity()})
 
+#send json data using MQTT
 def send():
     CommInternet.SendJson(jsonify())
 

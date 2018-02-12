299b0af1521f2e7d5eff6c62b5e4d72baf89a024
Karrot96-L <apk115@ic.ac.uk>
2018-02-12 14:05:43 +0000

    fd

--- Website/js/MqttConnect.js
+++ Website/js/MqttConnect.js
@@ -1,40 +1,25 @@
-var mqtt;
-var reconnectTimeout = 2000;
-var host="192.168.0.10"; //change this
-//var host="iot.eclipse.org"
-//var port=80
-var port=9001;
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('app.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
 
-function onFailure(message) {
-    console.log("Connection Attempt to Host "+host+"Failed");
-    setTimeout(MQTTconnect, reconnectTimeout);
-}
-function onMessageArrived(msg){
-    out_msg="Message received "+msg.payloadString+"<br>";
-    out_msg=out_msg+"Message received Topic "+msg.destinationName;
-    console.log(out_msg);
 
-}
+var mqtt = require('mqtt')
+var client  = mqtt.connect('http://192.168.0.10')
 
-function onConnect() {
-// Once a connection has been made, make a subscription and send a message.
+client.on('connect', function () {
+  client.subscribe('ESYS/netball')
+  //client.publish('ESYS/netball', '')
+})
 
-console.log("Connected ");
-mqtt.subscribe("ESYS/netball");
-//message = new Paho.MQTT.Message("Hello World");
-//message.destinationName = "sensor1";
-//mqtt.send(message);
-}
-function MQTTconnect() {
-console.log("connecting to "+ host +" "+ port);
-mqtt = new Paho.MQTT.Client(host,port,"clientjs");
-//document.write("connecting to "+ host);
-var options = {
-    timeout: 3,
-    onSuccess: onConnect,
-    onFailure: onFailure,
-     };
-mqtt.onMessageArrived = onMessageArrived
-
-mqtt.connect(options); //connect
-}
+client.on('message', function (topic, message) {
+  // message is Buffer
+  var obj = JSON.parse(message);
+  console.log(obj.toString())
+  client.end()
+})

980506a6c1cf6cddfc791cbb64d44ebfcb4136cd
Karrot96-L <apk115@ic.ac.uk>
2018-02-12 14:50:22 +0000

    backend

--- Website/js/MqttConnect.js
+++ Website/js/MqttConnect.js
@@ -1,16 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('app.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
-
-
 var mqtt = require('mqtt')
 var client  = mqtt.connect('http://192.168.0.10')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
 
 client.on('connect', function () {
   client.subscribe('ESYS/netball')
@@ -20,6 +12,25 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   var obj = JSON.parse(message);
-  console.log(obj.toString())
+  console.log(obj.value.toString())
   client.end()
+})
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+  client.publish('ESYS/netball', '{"name":"fog" , "value": "100" ,  "WindowFogged": "True", "RemoveFog": "True"}')
+
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })

b664b8d88ed5b9976875a1493c2f5c80afbb6ac2
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-02-12 11:11:31 +0000

    Update CommInternet.py

--- Code/CommInternet.py
+++ Code/CommInternet.py
@@ -2,14 +2,16 @@ from umqtt.simple import MQTTClient
 import machine
 import network
 
-
+#disable network
 def DisableAp():
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False)
 
+#connect to wifi
 def ConnectWifi():
     import network
     sta_if = network.WLAN(network.STA_IF)
+    #check connection and if not connected, connect using username and password
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
@@ -21,10 +23,12 @@ def ConnectWifi():
         print('Not connected to network...')
     print('network config:', sta_if.ifconfig())
 
+#send json data to MQTT
 def SendJson(data):
-    #ConnectWifi()
     CLIENT_ID = machine.unique_id()
+    #define IP
     BROKER_ADDRESS =  '192.168.0.10'
+    #define topic to filter
     TOPIC = "ESYS/netball"
     client = MQTTClient(CLIENT_ID,BROKER_ADDRESS)
     client.connect()
@@ -32,6 +36,7 @@ def SendJson(data):
     client.disconnect()
     print("Published")
 
+#check wifi connection and return boolean
 def CheckConnection():
     import network
     sta_if = network.WLAN(network.STA_IF)

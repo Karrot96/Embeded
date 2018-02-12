cfd857315d932d018848515da3302853bb6a7f66
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-02-12 12:33:42 +0000

    Update Json.py

--- Code/Json.py
+++ Code/Json.py
@@ -4,8 +4,9 @@ import ujson as json
 
 #convert data to json
 def jsonify():
-    return json.dumps({'name':'humid1' , 'value': Sensor.humidity()})
-
+    return json.dumps({'name':'humid1' , 'value': Sensor.humidity() , '})
+    return json.dumps({'name':'fog' , value:'Sensor.humidity()' ,  'WindowFogged': Sensor.fog(), 'RemoveFog'})
+                       
 #send json data using MQTT
 def send():
     CommInternet.SendJson(jsonify())

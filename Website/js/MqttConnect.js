07decffab1e7d605b86396a742881ad52e685aa1
Karrot96-L <apk115@ic.ac.uk>
2018-02-13 15:54:52 +0000

    commenting

--- Website/js/MqttConnect.js
+++ Website/js/MqttConnect.js
@@ -1,62 +1,83 @@
+//Node.js module
+
+
+//Node.js mqtt module
 var mqtt = require('mqtt')
 var client  = mqtt.connect('http://192.168.0.10')
+
+//Node.js express Module
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
-var obj = JSON.parse('{"RemoveFog": "True", "WindowFogged": "false", "name":"fog" , "value": "100" }')
+
+//Node.js notifier module
 const notifier = require('node-notifier');
 const path = require('path');
+
+//Node.js opn module
 const opn = require('opn');
 
 
+//Global Json variable for passing data around the device, initialised on server Starting
+//Would be quickly overwritten as soon as a device starts transmitting data
+var obj = JSON.parse('{"RemoveFog": "True", "WindowFogged": "false", "name":"fog" , "value": "100" }')
+
+
+//Connect to the MQTT broker
 client.on('connect', function () {
-  client.subscribe('ESYS/netball')
-  //client.publish('ESYS/netball', '')
+    //subscribe to the correct Topic
+    client.subscribe('ESYS/netball')
 })
 
-
+//When the broker gets updated
 client.on('message', function (topic, message) {
-  // message is Buffer
-  obj = JSON.parse(message);
-  console.log(obj.WindowFogged.toString())
-  if (obj.WindowFogged && !obj.RemoveFog){
-      notifier.notify({
-        title: 'FogMeister',
-        message: 'Hello, Your car windscreen is currently fogged up',
-        icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-        sound: true, // Only Notification Center or Windows Toasters
-        wait: true // Wait with callback, until user action is taken against notification
-      }, function (err, response) {
+    //global json object is updated to hold the message uploaded to the broker topic
+    obj = JSON.parse(message);
+    console.log(obj.WindowFogged.toString())
+
+    //The data from the broker is analysed
+    //Different notifications are posted depending on the condition of the car
+    if (obj.WindowFogged && !obj.RemoveFog){
+        notifier.notify({
+            title: 'FogMeister',
+            message: 'Hello, Your car windscreen is currently fogged up',
+            icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+            sound: true, // Only Notification Center or Windows Toasters
+            wait: true // Wait with callback, until user action is taken against notification
+        }, function (err, response) {
         // Response is response from notification
-      });
-  }else if (obj.RemoveFog){
-      notifier.notify({
-        title: 'FogMeister',
-        message: 'Your car it now demisting',
-        icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-        sound: true, // Only Notification Center or Windows Toasters
-        wait: true // Wait with callback, until user action is taken against notification
-      }, function (err, response) {
-        // Response is response from notification
-      });
-  }else if (!obj.WindowFogged){
-      notifier.notify({
-        title: 'FogMeister',
-        message: 'Your car it no longer fogged up',
-        icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-        sound: true, // Only Notification Center or Windows Toasters
-        wait: true // Wait with callback, until user action is taken against notification
-      }, function (err, response) {
-        // Response is response from notification
-      });
-  }
-  //client.end()
+        });
+    }else if (obj.RemoveFog){
+        notifier.notify({
+            title: 'FogMeister',
+            message: 'Your car it now demisting',
+            icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+            sound: true, // Only Notification Center or Windows Toasters
+            wait: true // Wait with callback, until user action is taken against notification
+        }, function (err, response) {
+            // Response is response from notification
+        });
+    }else if (!obj.WindowFogged){
+        notifier.notify({
+            title: 'FogMeister',
+            message: 'Your car it no longer fogged up',
+            icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+            sound: true, // Only Notification Center or Windows Toasters
+            wait: true // Wait with callback, until user action is taken against notification
+        }, function (err, response) {
+            // Response is response from notification
+        });
+    }
 })
+
+
+//Checks if the notification timesout
 notifier.on('timeout', function() {
   console.log('Timed out!');
+  //If car is fogged dont let the timeout remove the notification
   if (obj.WindowFogged && !obj.RemoveFog){
       notifier.notify({
         title: 'FogMeister',
@@ -69,13 +90,15 @@ notifier.on('timeout', function() {
       });
   }
 });
+
+//when clicking on the notification open up the browser
 notifier.on('click', (obj, options) => {
     opn('http://localhost:8080');
 });
 
+
+//On browser refresh determine what stage we are in and display corresponding page
 app.get('/', function (req, res) {
-  //var client  = mqtt.connect('http://192.168.0.10')
-  //client.subscribe('ESYS/netball')
   console.log(obj.WindowFogged)
   if ((obj.WindowFogged.toString() == "true") && (obj.RemoveFog.toString() == "false")){
       res.render('index', {display:"Demist"})

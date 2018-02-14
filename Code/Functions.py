def87f5f382506dd7be0605f7caf83e2a5a6643c
sophia-ash <35796908+sophia-ash@users.noreply.github.com>
2018-02-14 00:25:17 +0000

    Update Functions.py

--- Code/Functions.py
+++ Code/Functions.py
@@ -69,27 +69,6 @@ def jsonify():
 def send():
     SendJson(jsonify())
     
-def sub_cb(topic, msg):
-    print((topic, msg))
-
-def subscribe(server="localhost"):
-    c = MQTTClient(machine.unique_id(), '192.168.0.10')
-    c.set_callback(sub_cb)
-    c.connect()
-    c.subscribe("ESYS/netball")
-    while True:
-        if True:
-            # Blocking wait for message
-            c.wait_msg()
-        else:
-            # Non-blocking wait for message
-            c.check_msg()
-            # Then need to sleep to avoid 100% CPU usage (in a real
-            # app other useful actions would be performed instead)
-            time.sleep(1)
-
-    c.disconnect()
-    
 #disable network
 def DisableAp():
     ap_if = network.WLAN(network.AP_IF)
